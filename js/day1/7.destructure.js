8cd6857c418dcb91e098d86f8d3b859ab8969f77
seohee99 <imshee99@gmail.com>
2024-01-22 16:46:46 +0900

    [Feat] Javascript 기본 문법 실습

--- js/day1/7.destructure.js
+++ js/day1/7.destructure.js
@@ -11,3 +11,15 @@ let [v1, v2, ...v3] = arr2;
 console.log(v1); // a
 console.log(v2); // b
 console.log(v3); // ['c', 'd']
+
+let options = {
+    title : "menu",
+    width : 100,
+    height : 200,
+    k2 : 'v2',
+};
+
+let {height, title:t,...rest} = options;
+console.log(height);
+console.log(t);
+console.log(rest);

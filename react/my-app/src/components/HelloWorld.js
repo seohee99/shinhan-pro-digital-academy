afbdf5f98063c2c96a706d825ad3ea93b3b8c463
seohee99 <imshee99@gmail.com>
2024-01-23 14:45:59 +0900

    [Feat] react 실습 - 간단한 문구

---
+++ react/my-app/src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, World!!</h1>
+            <p>This is seohee's first React Application</p>
+        </div>
+    )
+}

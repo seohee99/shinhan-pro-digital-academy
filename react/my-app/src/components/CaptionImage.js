f9f785ca57b69888c304077b020be2e3b9d35d93
seohee99 <imshee99@gmail.com>
2024-01-23 15:21:23 +0900

    [Feat] react 실습 - props 전달

--- react/my-app/src/components/CaptionImage.js
+++ react/my-app/src/components/CaptionImage.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function CaptionImage(props){
     return (
         <div style={{textAlign:'center'}}>
-            <img src={props.imgUrl} alt={props.caption}  />
+            <img style={{width:200}} src={props.imgUrl} alt={props.caption}  />
             <p> {props.caption} </p>
 
-            <img src="https://m.pororomall.com/web/product/big/202110/c3efd149cf7032c4582f56d8b5c3e6ff.jpg"></img>
+            <img style={{width:200}} src="https://m.pororomall.com/web/product/big/202110/c3efd149cf7032c4582f56d8b5c3e6ff.jpg"></img>
             <p>이건 트럭이에요</p>
         </div>
     )

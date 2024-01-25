a4b912a4888361db1ccab0cad1f61a84326023ff
seohee99 <imshee99@gmail.com>
2024-01-25 10:10:35 +0900

    [Feat] ToDoApp 샘플 코드

---
+++ react/todo_sample/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './components/ToDoApp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp></TodoApp>
+    </div>
+  );
+}
+
+export default App;

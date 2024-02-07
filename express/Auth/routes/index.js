eac816dacdbbe79d580c7ff0673c02a5b97b7503
seohee99 <imshee99@gmail.com>
2024-02-07 11:09:03 +0900

    [Feat] Cookie & Session 실습

---
+++ express/Auth/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.cookie("cookieName", "cookieValue", {
+    httpOnly : true
+  });
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
---
+++ express/Auth/routes/profile.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+router.get("/:paramId", (req,res,next) => {
+    console.log(req.session);
+    
+    if (!req.session.viewCount) {
+        req.session.viewCount = 0;
+    }
+    req.session.viewCount++;
+
+    if (!req.session.path){
+        req.session.parh = [];
+    }
+    console.log(req.params.paramId);
+    req.session.path.push(req.params.paramId);
+    
+    console.log("View Count ::",req.session.viewCount);
+    res.send("session test");
+});
+
+module.exports = router;

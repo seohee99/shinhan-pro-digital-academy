90f70be565b363636da1ec010e1ef458545048cc
seohee99 <imshee99@gmail.com>
2024-02-19 10:36:58 +0900

    [Feat] createdAt, updatedAt 형식 변경을 위한 함수 정의

---
+++ react/my-react-router/src/lib/hooks/FormattedDate.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+
+export default function formatDate(timeString) {
+    const date = new Date(timeString);
+  const year = String(date.getFullYear()).slice(2); 
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
+}

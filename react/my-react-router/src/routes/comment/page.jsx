a27310d04a3603a9f2b136cd9c6d6f9056f7c2e6
seohee99 <imshee99@gmail.com>
2024-02-19 10:37:27 +0900

    [Feat] comment CRD 완성, update 작성중

--- react/my-react-router/src/routes/comment/page.jsx
+++ react/my-react-router/src/routes/comment/page.jsx
@@ -1,18 +1,18 @@
-import { React, useEffect, useState } from 'react';
-import { Container, Card, Button, PencilFill, Trash3Fill , Form} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Card, Button,Form } from 'react-bootstrap';
+import { Pencil, Trash3 } from 'react-bootstrap-icons';
 import { deleteComment, fetchCommentList, updateComment } from '~/lib/apis/comment';
 import CommentWritePage from './write/page';
+import formatDate from '~/lib/hooks/FormattedDate';
 
-export default function CommentListPage({ boardId }) {
+export default function CommentListPage({ board }) {
+  console.log("boardId :: ", board._id)
   const [user, setUser] = useState(null);
   const [commentList, setCommentList] = useState([]);
   const [isEdit, setIsEdit] = useState(false);
   const [content, setContent] = useState('');
-
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+  const boardId = board._id
+  console.log("boardID ::", boardId)
 
   // localStorage에서 user 정보 불러오기
   useEffect(() => {
@@ -29,57 +29,73 @@ export default function CommentListPage({ boardId }) {
       setCommentList(data);
     };
     fetchData();
-  }, [])
+  }, [boardId])
 
   const onEdit = () => {
     setIsEdit(true);
   }
 
-  const onDelete = (boardId) => {
+  const onDelete = (commentId) => {
     if (confirm("해당 댓글을 삭제하시겠습니까?")) {
-      deleteComment({boardId});
+      deleteComment({ commentId });
       alert("삭제되었습니다.");
       window.location.reload();
     }
   }
-  
 
   const onSubmit = (boardId) => {
-    updateComment({ boardId,  content });
+    updateComment({ boardId, content });
     setIsEdit(false);
   }
-
 
   return (
     <>
       <div>Comment</div>
       <Container>
+
         {commentList.map((comment, index) => {
-          <Card key={index} style={{ width: '500px', 'backgroundColor': '#F5F5F5', 'border': 'none' }}>
+          const createdAt = formatDate(comment.createdAt);
+          const updatedAt = formatDate(comment.updatedAt);
+          return (
+            <Card key={index} style={{ width: '800px', 'backgroundColor': '#F5F5F5', 'border': 'none' , 'margin':15}}>
 
-            <Card.Body>
-              <Card.Title>
-                🏷️ 댓글 {index}
-                {comment.author === user._id && (
-                  <span style={{ float: 'right' }}>
-                    {
-                      !isEdit && <Button onClick={onEdit} style={{ backgroundColor: 'white', border: 'none', padding: 0, marginRight: 10 }}><PencilFill style={{ fontSize: '1.5rem', color: 'gray' }} /></Button>
-                    }
-                    <Button onClick={() => onDelete(comment._id)} style={{ backgroundColor: 'white', border: 'none', padding: 0, marginRight: 10 }}><Trash3Fill style={{ fontSize: '1.5rem', color: 'gray' }} /></Button>
-                  </span>
-                )
-                }
-              </Card.Title>
+              <Card.Body>
+                <Card.Title>
+                  🏷️ 댓글 {index + 1} 
+                  {comment.author === user._id && (
+                    <span style={{ float: 'right' }}>
+                      {
+                        !isEdit && <Button onClick={onEdit} style={{ backgroundColor: 'white', border: 'none', padding: 0, marginRight: 10 }}><Pencil style={{ fontSize: '1.5rem', color: 'gray' }} /></Button>
+                      }
+                      <Button onClick={() => onDelete(comment._id)} style={{ backgroundColor: 'white', border: 'none', padding: 0, marginRight: 10 }}><Trash3 style={{ fontSize: '1.5rem', color: 'gray' }} /></Button>
+                    </span>
+                  )
+                  }
+                </Card.Title>
+                <Card.Body>
+                  {isEdit ? (
+                    <Form.Control as='textarea' rows={5} value={content} onChange={(e) => setContent(e.target.value)}></Form.Control>
+                  ):(
+                    <>
+                  <div style={{'padding' : 10}}> Written By : {user.nickname} &nbsp; &nbsp; 📌 등록일 : {createdAt} &nbsp; &nbsp; 📌 수정일 : {updatedAt} </div>
+                  
+                  <div style={{'padding' : 10}}>  {comment.content} </div>
+                  </>
+                  )}
+                </Card.Body>
 
-            </Card.Body>
+              </Card.Body>
 
-          </Card>
+            </Card>
+          )
         })}
       </Container>
 
-      {user && (
-            <CommentWritePage ></CommentWritePage>
-            )}
+      <Container>
+        {user && (
+          <CommentWritePage boardId={boardId} index={commentList.length + 1} ></CommentWritePage>
+        )}
+      </Container>
     </>
   )
 }
--- react/my-react-router/src/routes/comment/write/page.jsx
+++ react/my-react-router/src/routes/comment/write/page.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Card, Button, Form } from 'react-bootstrap';
+import { PencilFill } from 'react-bootstrap-icons';
 
+import { createComment } from '~/lib/apis/comment';
 
-export default function CommentWritePage() {
+export default function CommentWritePage({index, boardId}) {
+  const [content, setContent] = useState('');
+
+  const onSubmit = useCallback(async (e) => {
+    console.log(boardId)
+    const response = await createComment({boardId, content})
+    
+    alert("댓글이 등록되었습니다.");
+    window.location.reload();
+    
+  },[boardId, content])
+
   return (
     <>
-      <Button class="btn" style={{ 'backgroundColor': 'pink', 'border': 'none', 'float': 'right', 'margin': '10px' }} onClick={handleShow} >
-                        글 작성하기
-                    </Button>
+      <Form onSubmit={onSubmit}>
+      <Card style={{ width: '800px', 'backgroundColor': '#FFFFFF', 'border': 'solid' , 'marginTop':30, 'padding':20}}>
+        <Card.Title>  
+        ✏️ {index}번째 댓글 작성중 . . . 
+        </Card.Title>
+        <Card.Body>
+          <Form.Control as='textarea' rows={5} placeholder='댓글을 입력하세요!' value={content} onChange={(e) => setContent(e.target.value)}></Form.Control>
+        <Button class="btn" onClick={() => onSubmit()} style={{ 'backgroundColor': 'pink', 'border': 'none', 'float': 'right', 'margin': '10px' }}  >
+          등록
+        </Button>
+        </Card.Body>
+      </Card>
+      </Form>
     </>
   )
 }
